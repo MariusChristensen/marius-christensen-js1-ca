bde1c4662ef7f9ad49b34cd229209b8ebc3ad9af
MariusChristensen <91680871+MariusChristensen@users.noreply.github.com>
2022-02-21 18:25:50 +0100

    Started working on index page.
    Got my HTML for index sorted. Need to link it to a details page and link to specific objects. Try to remember making them dynamic and reusable.
    
    After that it's making the form for my contact page and linking index<->contact

---
+++ js/script.js
@@ -0,0 +1,34 @@
+const url = "https://api.punkapi.com/v2/beers";
+
+const beerContainer = document.querySelector(".beers");
+
+async function fetchBeers() {
+  try {
+    const response = await fetch(url);
+    const json = await response.json();
+    const foods = json;
+
+    beerContainer.innerHTML = "";
+
+    for (let i = 0; i < foods.length; i++) {
+      if (i === 8) {
+        break;
+      }
+
+      const beerName = foods[i].name;
+      const tagline = foods[i].tagline;
+      const firstbrewed = foods[i].first_brewed;
+
+      beerContainer.innerHTML += `<div class="card">
+                                  <h4>${beerName}</h4>
+                                  <p>${tagline}</p>
+                                  <p>First brewed: ${firstbrewed}</p>
+                                  </div>`;
+    }
+  } catch (error) {
+    console.log("This error has occurred: " + error);
+    beerContainer.innerHTML = "This happened while trying to reach the API " + error;
+  }
+}
+
+fetchBeers();
