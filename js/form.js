a3c6eb92a16b01c239b1a11596a6368a12df8679
MariusChristensen <91680871+MariusChristensen@users.noreply.github.com>
2022-02-26 15:01:43 +0100

    All done
    - Figured out level 2 after a LOT of tinkering, turns out it was pretty easy.
    - Couldn't have a border on my success message due to it displaying even when the div was empty, need to figure that out in the future.

--- js/form.js
+++ js/form.js
@@ -7,32 +7,46 @@ const email = document.querySelector("#email");
 const emailError = document.querySelector("#email-error");
 const address = document.querySelector("#address");
 const addressError = document.querySelector("#address-error");
+const submit = document.querySelector("#submit-button");
+const successMessage = document.querySelector(".success-message");
 
 function validateForm(event) {
   event.preventDefault();
+
+  let hasErrors = false;
 
   if (fullName.value.trim().length > 0) {
     fullNameError.style.display = "none";
   } else {
     fullNameError.style.display = "block";
+    hasErrors = true;
   }
 
   if (subject.value.trim().length >= 10) {
     subjectError.style.display = "none";
   } else {
     subjectError.style.display = "block";
+    hasErrors = true;
   }
 
   if (validateEmail(email.value) === true) {
     emailError.style.display = "none";
   } else {
     emailError.style.display = "block";
+    hasErrors = true;
   }
 
   if (address.value.trim().length >= 25) {
     addressError.style.display = "none";
   } else {
     addressError.style.display = "block";
+    hasErrors = true;
+  }
+
+  if (hasErrors) {
+    successMessage.innerHTML = "";
+  } else {
+    successMessage.innerHTML = `<h2>Thank you for submitting, ${fullName.value}!</h2>`;
   }
 }
 
